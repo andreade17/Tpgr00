4b7c36678b7866a6a205a5e9a9e83ad14c95b570
andreade17 <andreade2010@gmail.com>
2017-08-15 17:30:43 -0300

    sss

---
+++ SamplexaGIT/src/logica/Propuesta.java
@@ -0,0 +1,14 @@
+package logica;
+
+public class Propuesta {
+
+	private String nombre;
+	
+	 public String getNombre() {
+	        return nombre;
+	    }
+	 
+	  public void setNombre(String n) {
+	        nombre = n;
+	    }
+}
